3eacc843739c5e4eb79f16c0a843b183bbe23cae
GitHub Action <action@github.com>
2024-09-18 11:39:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/mihalypal/biroplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mihalypal/biroplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.mihalypal.biroplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mihalypal.biroplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
